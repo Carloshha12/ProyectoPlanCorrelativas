6d10f04efd8d9a2bbc013e3c93621f6d3e85767f
Carlos Habiague <carlos.habiague@est.fi.uncoma.edu.ar>
2025-09-30 11:34:12 -0300

    Puse comentarios en las partes mas importantes

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 // funcion para mostrar el detalle de una materia
 function verDetalle(nombre, estado) {
   document.getElementById('detalleTitulo').textContent = nombre;
+  //En la card oculta tenemos el id detalleTitulo y le agregamos con textContent el nombre que recibimos por parametro
   document.getElementById('detalleEstado').textContent = "Estado: " + estado;
   document.getElementById('detalleMateria').classList.remove('oculto');
+  //removemos la clase oculto para que se muestre el detalle
 }
 
 function cerrarDetalle() {
   document.getElementById('detalleMateria').classList.add('oculto');
+  //agregamos la clase oculto para que se oculte el detalle
 }
