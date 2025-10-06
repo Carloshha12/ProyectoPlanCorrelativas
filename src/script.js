7e80994c245637a79ef049cf3c89bfe116c8f7f2
JuanCruzOliva <juancruz.oliva@est.fi.uncoma.edu.ar>
2025-10-06 15:02:31 -0300

    Ya no se crea el detalle en el index, todo en el json, arregle lo del color aprobado desaprobado, pruebas de cambiar el estado de las materias en el json entre otras

--- src/script.js
+++ src/script.js
@@ -3,13 +3,21 @@ fetch('materias.json').then(res => res.json()).then(materias => { materiasjson =
 
 
 // funcion para mostrar el detalle de una materia
-function verDetalle(nombre, estado) {
+function verDetalle(estado, nombre, id) {
+  
+  const detalle = document.querySelector('#detalleMateria');
+  const card = document.createElement('div');
+        card.innerHTML =`<div class="detalle">
+            <h3 id="detalleTitulo">${nombre}</h3>
+            <p id="detalleEstado">Estado: ${estado}</p>
+            <button onclick="cursar(${String.estado}, ${id})" class="btn cursada">Cursada</button>
+            <button onclick="aprobar(${String.estado}, ${id})" class="btn aprobada">Aprobada</button>
+            <button onclick="cerrarDetalle()" class="btn">Cerrar</button>
 
-  document.getElementById('detalleTitulo').textContent = `${nombre}`;
-  //En la card oculta tenemos el id detalleTitulo y le agregamos con textContent el nombre que recibimos por parametro
-  document.getElementById('detalleEstado').textContent = "Estado: " + estado;
-  document.getElementById('detalleMateria').classList.remove('oculto');
-  //removemos la clase oculto para que se muestre el detalle
+        </div>`
+
+        detalle.appendChild(card);
+        document.getElementById('detalleMateria').classList.remove('oculto');
 }
 function verCorrelativa(id, correlativa){
 hacer=false;
@@ -18,16 +26,30 @@ if(materiasjson.primeraño[id-1].correlativas==null){
 }
 return hacer;
 }
-
+function agregarMateria() {
+const root = document.querySelector('#materiasContainer');
+  const card = document.createElement('div');
+    //aca se le puede insertar etiquetas html
+    card.innerHTML = `<div class="materiasGrilla">
+            <div class="card pendiente" id="${materiasjson.primeraño[4].id}" onclick="verDetalle('${materiasjson.primeraño[4].nombre}', 'Aprobada')">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <h4>ID: ${materiasjson.primeraño[4].id}</h4>
+                    <h2 class="card-title">${materiasjson.primeraño[4].nombre}</h2>
+                    <p id="estado"class="card-status">Estado: ${materiasjson.primeraño[4].estado}</p>
+                    </div>
+                    </div>
+      `
+    root.appendChild(card);
+}
 function mostrarMaterias() {
-  console.log(materiasjson.primeraño[0].nombre);
   const root = document.querySelector('#materiasContainer');
   Array.from(materiasjson.primeraño).forEach(materia => {
     if(verCorrelativa(materia.id)){
     const card = document.createElement('div');
     //aca se le puede insertar etiquetas html
     card.innerHTML = `<div class="materiasGrilla">
-            <div class="card pendiente" id="${materia.id}" onclick="verDetalle('${materia.nombre}', 'Aprobada')">
+            <div class="card pendiente" id="${materia.id}" onclick="verDetalle('${materia.estado}', '${materia.nombre}', '${materia.id}')">
                 <div class="card-header"></div>
                 <div class="card-body">
                     <h4>ID: ${materia.id}</h4>
@@ -35,36 +57,35 @@ function mostrarMaterias() {
                     <p id="estado"class="card-status">Estado: ${materia.estado}</p>
                     </div>
                     </div>
-                      <div id="detalleMateria" class="detalle oculto">
-            <h3 id="detalleTitulo"></h3>
-            <p id="detalleEstado"></p>
-            <button onclick="cursar(${materia.id})" class="btn cursada">Cursada</button>
-            <button onclick="aprobar(${materia.id})" class="btn aprobada">Aprobada</button>
-            <button onclick="cerrarDetalle()" class="btn">Cerrar</button>
-
-        </div>
       `
-      document.getElementById
     root.appendChild(card);//guardamos la card en el root
 }})
 
 }
-function cursar(id) {
-  var element = document.getElementById(1).classList;
-  element.remove('card', 'pendiente');
+function cursar(estado, id) {
+  console.log(id);
+  var element = document.getElementById(id).classList;
+  element.remove('card', 'aprobada');
+element.remove('card', 'pendiente');
+element.remove('card', 'cursada');
+  materiasjson.primeraño[id-1].estado="cursada";
   element.add('card', 'cursada');
   document.getElementById('estado').textContent="Estado: Cursada";
   document.getElementById('detalleMateria').classList.add('oculto');
   
+  
   //agregamos la clase oculto para que se oculte el detalle
 }
-function aprobar(id) {
-  var element = document.getElementById(1).classList;
+function aprobar(estado, id) {
+  console.log(id);
+  estado
+   var element = document.getElementById(id).classList;
   element.remove('card', 'pendiente');
+  element.remove('card', 'cursada');
+  element.remove('card', 'aprobada');
   element.add('card', 'aprobada');
   document.getElementById('estado').textContent="Estado: Aprobada";
   document.getElementById('detalleMateria').classList.add('oculto');
-  
   //agregamos la clase oculto para que se oculte el detalle
 }
 function cerrarDetalle() {
